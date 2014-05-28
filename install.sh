a650d24a221d3c5b24978b0a1c93ba7627b0c2d4
hayzer <hayzer@gmail.com>
2014-05-28 15:00:31 +0200

    More debug

--- install.sh
+++ install.sh
@@ -5,10 +5,14 @@ source=my-dot-files.tar.gz
 
 (
 	cd /tmp
+	echo * Downloading..
 	curl -s -L -o ${source} http://github.com/hayzer/my-dot-files/tarball/master
+	echo * Opening archive..
 	tar zxf ${source}
+	echo * Doing something..
 	cd hayzer-*/
-	pwd
+	path=$(pwd)
+	echo * The path is ${path}
 	ls -la
 	cd -
 	rm -fr hayzer-*/ ${source}
