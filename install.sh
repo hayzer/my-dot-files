e4e5c602050d487314fffeabe0f5ee9bee61ad81
thomas <thomas@localhost.localdomain>
2014-05-28 14:56:56 +0200

    Just debug installer

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # install my dot files locally
+set +x
 home=${HOME}
 source=my-dot-files.tar.gz
 
@@ -7,7 +8,10 @@ source=my-dot-files.tar.gz
 	curl -s -L -o ${source} http://github.com/hayzer/my-dot-files/tarball/master
 	tar zxf ${source}
 	cd hayzer-*/
+	pwd
 	ls -la
 	cd -
 	rm -fr hayzer-*/ ${source}
 )
+
+set -x
